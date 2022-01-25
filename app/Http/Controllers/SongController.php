dfdafb79d37e476fbe47fd0336b2f744b88d3c5c
Chetan-shinde <chetanshinde120@gmail.com>
2022-01-25 20:22:32 +0530

    song fetched

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Song;
 
 class SongController extends Controller
 {
     public function getSongs(){
-    	return response()->json(["success"=>true, "songs"=>[]]);
+    	$songs = Song::getSongs();
+    	return response()->json(["success"=>true, "songs"=>$songs);
     }
 }
